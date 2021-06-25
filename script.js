361e9bc0fb2754ebdd1f29dcdf5ca1f29170b70c
Thammineni <mithun.ram.kumar.thammineni@sap.com>
2021-06-25 13:00:40 +0530

    day 4

---
+++ script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    //runs after entire website is loaded
+    let cellContainer = $(".input-cell-conainer");
+    for (let i = 1; i <= 100; i++) {
+        let ans = "";
+
+        let n = i;
+
+        while (n > 0) {
+            let rem = n % 26;
+            if (rem == 0) {
+                ans = "Z" + ans;
+                n = Math.floor(n / 26) - 1;
+
+            }
+            else {
+                ans = String.fromCharCode(rem - 1 + 65) + ans;
+                n = Math.floor(n / 26);
+            }
+        }
+
+        let column = $(`<div class="column-name colId-${i}" id="colCod-${ans}">${ans}</div>`);
+        $(".column-name-container").append(column);
+
+    }
+
+
+
+});
+
