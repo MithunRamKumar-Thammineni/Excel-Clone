6a8d8e6b1f250a3e014327bcf83d04e0c4736a85
Thammineni <mithun.ram.kumar.thammineni@sap.com>
2021-07-02 08:48:33 +0530

    Day 5

--- script.js
+++ script.js
@@ -48,12 +48,12 @@ $(document).ready(function () {
 
     }
 
-    for (let i = 1; i < 100; i++) {
-        let row = $(`<div class="cell-row"></div>`)
+    for (let i = 1; i <= 100; i++) {
+        let row = $(`<div class="cell-row"></div>`);
         for (let j = 1; j <= 100; j++) {
             let colCode = $(`.colId-${j}`).attr("id").split("-")[1];
-            let column = $(`<div class="input-cell" contenteditable="false" id = "row-{i}-col-${j}" data="code-A"></div>`)
-            row.append(column)
+            let column = $(`<div class="input-cell" contenteditable="false" id = "row-${i}-col-${j}" data="code-${colCode}"></div>`);
+            row.append(column);
         }
         $(".input-cell-container").append(row);
     }
@@ -67,10 +67,44 @@ $(document).ready(function () {
         $(this).toggleClass("selected");
     });
 
-    $(".input-cell").click(function () {
-        $(".input-cell.selected").removeClass("selected");
+    $(".input-cell").click(function (e) {
+        if (e.ctrlKey) {
+            let [rowId, colId] = getRowCol(this);
+            if (rowId > 1) {
+                let topCellSelected = $(`#row-${rowId - 1}-col-${colId}`).hasClass("selected");
+                if (topCellSelected) {
+                    $(this).addClass("top-cell-selected");
+                    $(`#row-${rowId - 1}-col-${colId}`).addClass("bottom-cell-selected");
+                }
+            }
+            if (rowId < 100) {
+                let bottomCellSelected = $(`#row-${rowId + 1}-col-${colId}`).hasClass("selected");
+                if (bottomCellSelected) {
+                    $(this).addClass("bottom-cell-selected");
+                    $(`#row-${rowId + 1}-col-${colId}`).addClass("top-cell-selected");
+                }
+            }
+            if (colId > 1) {
+                let leftCellSelected = $(`#row-${rowId}-col-${colId - 1}`).hasClass("selected");
+                if (leftCellSelected) {
+                    $(this).addClass("left-cell-selected");
+                    $(`#row-${rowId}-col-${colId - 1}`).addClass("right-cell-selected");
+                }
+            }
+            if (colId < 100) {
+                let rightCellSelected = $(`#row-${rowId}-col-${colId + 1}`).hasClass("selected");
+                if (rightCellSelected) {
+                    $(this).addClass("right-cell-selected");
+                    $(`#row-${rowId}-col-${colId + 1}`).addClass("left-cell-selected");
+                }
+            }
+        }
+        else {
+            $(".input-cell.selected").removeClass("selected");
+        }
         $(this).addClass("selected");
-    })
+
+    });
 
     $(".input-cell").dblclick(function () {
         $(".input-cell.selected").removeClass("selected");
@@ -78,6 +112,10 @@ $(document).ready(function () {
         $(this).attr("contenteditable", "true");
         $(this).focus();
     });
+
+    $(".input-cell").blur(function () {
+        $(".input-cell.selected").attr("contenteditable", "false");
+    })
 
     $(".input-cell-container").scroll(function () {
         $(".column-name-container").scrollLeft(this.scrollLeft);
@@ -90,4 +128,42 @@ $(document).ready(function () {
 
 
 });
+
+function getRowCol(ele) {
+    let idArray = $(ele).attr("id").split("-");
+    let rowId = parseInt(idArray[1]);
+    let colId = parseInt(idArray[3]);
+    return [rowId, colId];
+}
+
+function updateCell(property, value) {
+    $(".input-cell.selected").each(function () {
+        $(this).css(property, value);
+    })
+
+}
+
+$(".icon-bold").click(function () {
+    if ($(this).hasClass("selected")) {
+        updateCell("font-weight", "", true);
+    } else {
+        updateCell("font-weight", "bold", false);
+    }
+})
+
+$(".icon-italic").click(function () {
+    if ($(this).hasClass("selected")) {
+        updateCell("font-style", "", true);
+    } else {
+        updateCell("font-style", "italic", false);
+    }
+})
+
+$(".icon-underline").click(function () {
+    if ($(this).hasClass("selected")) {
+        updateCell("text-decoration", "", true);
+    } else {
+        updateCell("text-decoration", "underline", false);
+    }
+})
 
